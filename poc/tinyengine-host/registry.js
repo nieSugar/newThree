07c60227ab04dcbe2f3a674dad5d950c82ca9c03
Sugar <50561965+nieSugar@users.noreply.github.com>
2026-08-15 17:11:30 +0800

    feat: register real TinyEngine Three PoC toolbar

---
+++ poc/tinyengine-host/registry.js
@@ -0,0 +1,21 @@
+import { META_SERVICE, META_APP } from '@opentiny/tiny-engine-meta-register'
+import engineConfig from './engine.config.js'
+import { HttpService } from './src/composable/index.js'
+import ThreePocToolbar from './src/three-toolbar/index.js'
+
+export default {
+  [META_SERVICE.Http]: HttpService,
+  'engine.config': {
+    ...engineConfig
+  },
+  [ThreePocToolbar.id]: ThreePocToolbar,
+  [META_APP.Layout]: {
+    options: {
+      relativeLayoutConfig: {
+        [ThreePocToolbar.id]: {
+          insertAfter: META_APP.Refresh
+        }
+      }
+    }
+  }
+}
